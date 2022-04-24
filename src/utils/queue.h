e9cdd3273e822f040de118f936f763611eda5ebd
Hamidreza Rabbanian <hamidreza.rabbanian@gmail.com>
2022-04-25 00:25:58 +0430

    Changes code style

--- src/utils/queue.h
+++ src/utils/queue.h
@@ -1,51 +1,42 @@
-#ifndef HARF_QUEUE_H
-#define HARF_QUEUE_H
+#ifndef HARF_SRC_UTILS_QUEUE_H_
+#define HARF_SRC_UTILS_QUEUE_H_
 
 #include <condition_variable>
 #include <mutex>
 #include <queue>
 
-namespace utils
-{
+namespace utils {
 
 template <typename T>
-class queue
-{
-  private:
-    std::queue<T> buffer;
-    std::mutex mx;
-    std::condition_variable cv;
+class Queue {
+ public:
+  Queue() = default;
+  Queue(const Queue<T> &rhs) = delete;
 
-  public:
-    queue() = default;
-    queue(const queue<T> &rhs) = delete;
+  T Pop() {
+    std::lock_guard lock(mx_);
+    auto el = std::move(buffer_.front());
+    buffer_.pop();
+    return el;
+  }
 
-    T pop()
-    {
-        std::lock_guard lock(mx);
-        auto el = std::move(buffer.front());
-        buffer.pop();
-        return el;
-    }
+  void Push(const T &el) {
+    std::lock_guard lock(mx_);
+    buffer_.emplace(std::move(el));
+  }
 
-    void push(const T& el)
-    {
-        std::lock_guard lock(mx);
-        buffer.emplace(std::move(el));
-    }
+  void Push(T &&el) {
+    std::lock_guard lock(mx_);
+    buffer_.emplace(std::move(el));
+  }
 
-    void push(T&& el)
-    {
-        std::lock_guard lock(mx);
-        buffer.emplace(std::move(el));
-    }
+  auto Size() { return buffer_.size(); }
 
-    auto size()
-    {
-        return buffer.size();
-    }
+ private:
+  std::queue<T> buffer_;
+  std::mutex mx_;
 };
 
-} // namespace utils
+}  // namespace utils
 
-#endif // HARF_QUEUE_H
+#endif  // HARF_SRC_UTILS_QUEUE_H_
