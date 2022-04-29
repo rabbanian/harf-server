bcbf3dcc03a1308a776e93f6fbaa3288cb9d164b
Hamidreza Rabbanian <hamidreza.rabbanian@gmail.com>
2022-04-29 18:20:53 +0430

    Adds a Wait function to the thread safe Queue

--- src/utils/queue.h
+++ src/utils/queue.h
@@ -8,33 +8,60 @@
 namespace utils {
 
 template <typename T>
-class Queue {
+class Queue
+{
  public:
   Queue() = default;
   Queue(const Queue<T> &rhs) = delete;
 
-  T Pop() {
+  T Pop()
+  {
     std::lock_guard lock(mx_);
     auto el = std::move(buffer_.front());
     buffer_.pop();
     return el;
   }
 
-  void Push(const T &el) {
+  void Push(const T &el)
+  {
     std::lock_guard lock(mx_);
     buffer_.emplace(std::move(el));
+    cv_.notify_all();
   }
 
-  void Push(T &&el) {
+  void Push(T &&el)
+  {
     std::lock_guard lock(mx_);
     buffer_.emplace(std::move(el));
+    cv_.notify_all();
   }
 
-  auto Size() { return buffer_.size(); }
+  bool Wait()
+  {
+    while (Empty()) {
+      std::unique_lock lock(wmx_);
+      cv_.wait(lock);
+    }
+    return true;
+  }
+
+  bool Empty()
+  {
+    std::lock_guard lock(mx_);
+    return buffer_.empty();
+  }
+
+  auto Size()
+  {
+    std::lock_guard lock(mx_);
+    return buffer_.size();
+  }
 
  private:
   std::queue<T> buffer_;
   std::mutex mx_;
+  std::condition_variable cv_;
+  std::mutex wmx_;
 };
 
 }  // namespace utils
