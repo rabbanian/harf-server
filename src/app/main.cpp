48aa432dbc108e821f8c8103b93a3804bd8e929c
Hamidreza Rabbanian <hamidreza.rabbanian@gmail.com>
2022-05-01 17:32:42 +0430

    main.cpp: implements a simple echo server with Packets

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,76 +1,32 @@
-#include <cstdio>
-#include <cstring>
-#include <iostream>
-#include <string>
+#include <asio/signal_set.hpp>
 #include <thread>
 
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <unistd.h>
+#include "net/connection_manager.h"
+#include "net/server.h"
+#include "proto/echo.pb.h"
+#include "utils/queue.h"
 
-#include <asio.hpp>
-#include "db/database.h"
-
-void takeClient(asio::ip::tcp::socket socket)
+int main()
 {
-    auto &users = db::instance();
+  asio::io_context ioc;
+  asio::ip::tcp::endpoint endpoint(asio::ip::tcp::v4(), 1234);
 
-    std::string msg = "Enter your username: \n";
-    socket.write_some(asio::buffer(msg));
+  utils::Queue<std::pair<std::weak_ptr<net::internal::Connection>, net::Packet>>
+      queue;
+  net::ConnectionManager manager(queue);
+  std::make_shared<net::Server>(ioc, endpoint, manager)->Run();
 
-    socket.read_some(asio::buffer(msg));
+  asio::signal_set signals(ioc, SIGINT, SIGTERM);
+  signals.async_wait([&ioc](std::error_code const&, int) { ioc.stop(); });
 
-    users.insert(msg, fd);
-    dprintf(fd, "Welcome %s there are %d online user!\npress h for help!\n", buff, users.count());
-    std::cout << msg << "connected!" << std::endl;
+  std::thread([&ioc]() { ioc.run(); }).detach();
 
-    while (true)
-    {
-        char input[2];
-        if (0 == read(fd, &input, 2))
-        {
-            break;
-        }
-        if (input[0] == 'q')
-            break;
-        if (input[0] == 'h')
-        {
-            msg = "n: new massage\np: print online users\nq: exit\nh: help\n";
-            write(fd, msg.c_str(), msg.size());
-        }
-        if (input[0] == 'n')
-        {
-            char to[MAX_ID_LENGHT + 1];
-            to[MAX_ID_LENGHT] = '\0';
-            read(fd, to, MAX_ID_LENGHT);
-            int tofd;
-            if (users.is_registered(to))
-            {
-                tofd = users.get_fd(to);
-            }
-            else
-            {
-                std::cout << "name not found! (" << to << ")" << std::endl;
-                continue;
-            }
-            char buffer[1024];
-            int bytesRead = 0;
-            while ((bytesRead = read(fd, buffer, 1024)) > 0)
-                write(tofd, buffer, bytesRead);
-        }
+  while (queue.Wait()) {
+    auto [connection, packet] = queue.Pop();
+    if (!connection.expired()) {
+      auto sp = connection.lock();
+      sp->Send(packet);
     }
-    shutdown(fd, SHUT_RDWR);
-    close(fd);
-    users.remove_fd(fd);
-    std::cout << buff << "disconnected" << std::endl;
-}
-
-int main(int argc, char *argv[])
-{
-    asio::io_context context;
-    asio::ip::tcp::acceptor acceptor(context, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), 1234));
-
-    asio::ip::tcp::socket socket(acceptor.accept());
-
-    return 0;
+  }
+  return 0;
 }
